521946e060fc0f1c99910e52480ecab68df1744f
LewisBamf <bamfordlewis9@gmail.com>
2024-07-11 08:28:01 +0100

    chore: Update npm dependencies and add @vercel/analytics package

--- portfolio-site/src/routes/_layout.svelte
+++ portfolio-site/src/routes/_layout.svelte
@@ -1,6 +1,10 @@
 <script>
     import Navbar from '$lib/components/Navbar.svelte';
     import '../global.css';
+    import { dev } from '$app/environment';
+import { inject } from '@vercel/analytics';
+ 
+inject({ mode: dev ? 'development' : 'production' });
 </script>
 
 <Navbar />
